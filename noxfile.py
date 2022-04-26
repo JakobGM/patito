ed0c6d397093ddd23bcf1ac3f878f5e06d6d3a56
Jakob Gerhard Martinussen <10655778+JakobGM@users.noreply.github.com>
2022-04-26 19:50:10 +0200

    Use pinned dependencies for lint nox checks

--- noxfile.py
+++ noxfile.py
@@ -1,19 +1,38 @@
+import tempfile
+
 import nox
 
 nox.options.sessions = "lint", "test"
 locations = "src", "tests", "noxfile.py"
 
 
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--without-hashes",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python=["3.9"])
 def test(session):
-    session.run("poetry", "install", "-E", "duckdb", external=True)
-    session.run("pytest", "--cov")
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", "--no-dev", "-E", "duckdb", external=True)
+    install_with_constraints(session, "coverage[toml]", "pytest", "pytest-cov")
+    session.run("pytest", *args)
 
 
 @nox.session(python=["3.9"])
 def lint(session):
     args = session.posargs or locations
-    session.install(
+    install_with_constraints(
+        session,
         "flake8",
         "flake8-bandit",
         "flake8-black",
@@ -30,6 +49,6 @@ def lint(session):
 @nox.session(python="3.9")
 def format(session):
     args = session.posargs or locations
-    session.install("black", "isort")
+    install_with_constraints(session, "black", "isort")
     session.run("black", *args)
     session.run("isort", *args)
