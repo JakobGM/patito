c521461720d4518a4f2870f080ee21bfa8b1daca
Jakob Gerhard Martinussen <10655778+JakobGM@users.noreply.github.com>
2022-05-10 15:19:16 +0200

    Create a single source of truth for versioning

--- src/patito/__init__.py
+++ src/patito/__init__.py
@@ -33,3 +33,14 @@ try:
     ]
 except DistributionNotFound:  # pragma: no cover
     pass
+
+
+try:
+    from importlib.metadata import PackageNotFoundError, version  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
