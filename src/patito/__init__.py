1b682ee3ec0cf3e8ef5d8335c1ed880176c60021
Arkadius Schuchhardt <schuchhardt@auxmoney.com>
2023-07-22 18:17:43 +0200

    fix: added type annotations

--- src/patito/__init__.py
+++ src/patito/__init__.py
@@ -1,4 +1,6 @@
 """Patito, a data-modelling library built on top of polars and pydantic."""
+from typing import TypeVar, Type
+
 from polars import Expr, Series, col
 
 from patito import exceptions, sql
@@ -53,7 +55,11 @@ except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 
 
-def from_pydantic(pydantic_model: pydantic.BaseModel):
+ModelType = TypeVar("ModelType", bound=Model)
+PydanticModelType = TypeVar("PydanticModelType", bound=pydantic.BaseModel)
+
+
+def from_pydantic(pydantic_model: Type[PydanticModelType]) -> Type[ModelType]:
     class PydanticModelMixedWithPatito(pydantic_model, Model):
         pass
 
