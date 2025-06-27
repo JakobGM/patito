626f78f443a19a281fb5e6b6ca344d6c2ed62911
Thomas Aarholt <thomasaarholt@gmail.com>
2025-06-27 15:01:23 +0200

    Use all extras and groups

--- noxfile.py
+++ noxfile.py
@@ -19,8 +19,8 @@ locations = "src", "tests", "noxfile.py", "docs/conf.py"
 supported_python_versions = "3.9", "3.10", "3.11", "3.12"
 
 
-@session(python=supported_python_versions)
-def test(session):
+@session(python=supported_python_versions, uv_all_extras=True, uv_all_groups=True)
+def test(session: Session):
     """Run test suite using pytest + coverage + xdoctest."""
     if session.python == "3.9":
         # Only run test coverage and docstring tests on python 3.10
@@ -31,7 +31,7 @@ def test(session):
     session.run("pytest", *args, external=True)
 
 
-# @session(python="3.9")
+# @session(python="3.9", uv_all_extras=True, uv_all_groups=True)
 # def coverage(session):
 #     """Upload coverage data."""
 #     install_with_constraints(session, "coverage[toml]", "codecov")
@@ -39,7 +39,7 @@ def test(session):
 #     session.run("codecov", *session.posargs)
 
 
-@session(python=["3.9", "3.13"])
+@session(python=["3.9", "3.13"], uv_all_extras=True, uv_all_groups=True)
 def type_check(session: Session):
     """Run type-checking on project using pyright."""
     args = session.posargs or locations
@@ -47,7 +47,7 @@ def type_check(session: Session):
     session.run("mypy", *args, external=True)
 
 
-@session(python=["3.9", "3.13"])
+@session(python=["3.9", "3.13"], uv_all_extras=True, uv_all_groups=True)
 def lint(session: Session):
     """Run linters an project using flake8++."""
     args = session.posargs or locations
@@ -55,14 +55,14 @@ def lint(session: Session):
     session.run("ruff", "format", *args, external=True)
 
 
-@session(python="3.9")
+@session(python="3.9", uv_all_extras=True, uv_all_groups=True)
 def format(session: Session):
     """Run the ruff formatter on the entire code base."""
     args = session.posargs or locations
     session.run("ruff format", *args, external=True)
 
 
-@session(python="3.9")
+@session(python="3.9", uv_all_extras=True, uv_all_groups=True)
 def docs(session: Session) -> None:
     """Build the documentation."""
     if "--serve" in session.posargs:
